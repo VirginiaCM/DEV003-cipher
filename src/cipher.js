b938deb6fe25f32dfc67b738a75db9b245aa833d
virginia Castro <viniacm@hotmail.com>
2023-01-08 11:07:39 -0500

    segundo commit

--- src/cipher.js
+++ src/cipher.js
@@ -5,18 +5,42 @@ const cipher = {
     // condicional de los parámetros offset y text
     if(typeof posiciones !=='numero' || typeof text !== 'string') {
       throw new TypeError (); 
-      
+
+         
     }
 
     let actualPos = ""; // se crea una variable posiciones del alfabeto (0-65)
     let nuevaPos= ""; // se crea variable para aplicar la formula del algoritmo
     let textcodif= ""; // devuelve el resultado final
 
-     
+    //mediante un bucle for iteramos los elementos de text
+    for(let i=0; text.length; i++){
+      
+      actualPos = text.charAt(i);
+
+      //condicional para hallar posición ASCII
+      if(actualPos >= 65 && actualPos <= 90){
+        nuevaPos = (actualPos - 65 + posiciones) % 26 - 65; // aplicamos formula cipher
+
+      } else {
+        (actualPos < 65 || actualPos > 90);
+
+        nuevaPos += actualPos;
+      }
+
+      textcodif += String.fromCharCode (nuevaPos);
+    }
+
+    return textcodif;
+
+    
+    
+    
+      
     
   
   }
-  // ...
+  
 };
 
 export default cipher;
