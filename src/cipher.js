f8f0cea76d4d49b6dee843fa228aecd2bd7bf389
virginia Castro <viniacm@hotmail.com>
2023-01-08 10:49:38 -0500

    segundo commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,21 @@
 const cipher = {
+
+  encode : function (posiciones, text) {
+    
+    // condicional de los parámetros offset y text
+    if(typeof posiciones !=='numero' || typeof text !== 'string') {
+      throw new TypeError (); 
+      
+    }
+
+    let actualPos = ""; // se crea una variable posiciones del alfabeto (0-65)
+    let nuevaPos= ""; // se crea variable para aplicar la formula del algoritmo
+    let textcodif= ""; // devuelve el resultado final
+
+     
+    
+  
+  }
   // ...
 };
 
